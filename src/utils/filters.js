be0a9d57fd3cf7158578e4bd9a33e699e3ff7806
Andresv309 <andresv309@gmail.com>
2023-05-20 21:50:51 -0500

    Refactor FilterFunctions to be reusables

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -1,41 +1,110 @@
 import { slugify } from "./formatters"
 import { CATEGORIES_ICONS } from '../constants/icons'
 
-export async function filterPostsBy(allPosts, matchCondition) {
-  // const allPosts = await Astro.glob("../pages/post/*.md")
+const POST_FRONTMATTER_PAGES = {
+  CATEGORY: {
+    pageRoute: "category",
+    path: ["category"]
+  },
+  AUTHOR: {
+    pageRoute: "author",
+    path: ["author", "name"]
+  }
+}
 
-  const allMatches = await getUniqueMatches(allPosts, matchCondition)
+export async function getPagesFor(allPosts, matchCondition, filterPostsOptions) {
 
-  return allMatches.map(match => {
-    const matchPosts = allPosts.filter(post => post.frontmatter[matchCondition] === match)
+  const pageRoute = POST_FRONTMATTER_PAGES[matchCondition].pageRoute
+  const pagePath = POST_FRONTMATTER_PAGES[matchCondition].path
+
+  // const allUniquePages = await getUniquesForCondition(allPosts, matchCondition)
+  const allUniquePages = await getUniquesForCondition(allPosts, matchCondition)
+
+  return allUniquePages.map(page => {
+    // const matchPosts = allPosts.filter(post => post.frontmatter[matchCondition] === page)
+    const matchPosts = allPosts.filter(post => getValue(post.frontmatter, pagePath) === page)
+    const filteredPosts = filterBlogPosts(matchPosts, filterPostsOptions)
+
     return {
-      params: { [matchCondition]: slugify(match) },
+      params: { [pageRoute]: slugify(page) },
       props: {
-        [matchCondition]: match,
-        posts: matchPosts
+        [pageRoute]: page,
+        posts: filteredPosts
       }
     }
   })
 }
 
 export async function getMatchData(allPosts, matchCondition) {
-  const allMatches = await getUniqueMatches(allPosts, matchCondition)
+  const pageRoute = POST_FRONTMATTER_PAGES[matchCondition].pageRoute
 
-  return allMatches.map(match => (
+  const allUniqueValues = await getUniquesForCondition(allPosts, matchCondition)
+
+  return allUniqueValues.map(match => (
     {
       // [matchCondition]: match,
       name: match,
-      href: `${matchCondition}/${slugify(match)}`,
+      href: `${pageRoute}/${slugify(match)}`,
       icon: CATEGORIES_ICONS[match] ?? CATEGORIES_ICONS.DEFAULT
     }
   ))
 }
 
-async function getUniqueMatches(allPosts, matchCondition) {
+async function getUniquesForCondition(allPosts, matchCondition) {
+  const pagePath = POST_FRONTMATTER_PAGES[matchCondition].path
   return [
     ...new Set(allPosts.map(post => {
-      return post.frontmatter[matchCondition]
+      // return post.frontmatter[matchCondition]
+      return getValue(post.frontmatter, pagePath)
     }
     ).flat())
   ]
+}
+
+
+export function filterBlogPosts(posts, {
+  filterOutDrafts = true,
+  filterOutFuturePosts = true,
+  sortByDate = true,
+  limit = undefined,
+} = {}) {
+
+  // Filter draft and future posts
+  const filteredPosts = posts.reduce((acc, post) => {
+    const { date, draft } = post.frontmatter
+
+    if (filterOutDrafts && draft) return acc
+
+    if (filterOutFuturePosts && new Date(date) > new Date()) return acc
+
+    // Post passed the filter
+    acc.push(post)
+
+    return acc
+  }, [])
+
+  // Sort by date or randomize
+  if (sortByDate) {
+    filteredPosts.sort((a,b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date))
+  } else {
+    filteredPosts.sort(() => Math.random() - 0.5)
+  }
+
+  // console.log({limit, filteredPosts});
+
+  // Limit the number of posts passed
+  if (typeof limit === 'number') {
+    return filteredPosts.slice(0, limit)
+    // return filteredPosts.slice(limit)
+  }
+
+  return filteredPosts
+}
+
+function getValue(obj, keyPath) {
+  let value = obj;
+  keyPath.forEach(key => {
+    value = value[key]
+  })
+  return value
 }
