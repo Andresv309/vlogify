9a999b84cd36b6003c5ebf3f6c941c5e3f30ea5f
Andres <andresv309@gmail.com>
2023-05-23 21:39:49 -0500

    Add icons for new categories

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -5,5 +5,11 @@ export const CATEGORIES_ICONS = {
   'Productividad': 'mdi:lightbulb-on',
   'Desarrollo Personal': 'mdi:head-lightbulb',
   'Bienestar': 'mdi:flower-tulip-outline',
-  'Alimentación Saludable': 'mdi:food-apple'
+  'Alimentación Saludable': 'mdi:food-apple',
+  'Salud': 'mdi:hospital-box',
+  'Fitness': 'mdi:weight-lifter',
+  'Ciencia': 'mdi:molecule',
+  'Biología': 'mdi:dog',
+  'Medicina': 'mdi:test-tube',
+  'Computación': 'mdi:monitor-dashboard',
 }
